075b5ec1b33d95823ad47597ddb8a6e8e750c4a1
Hyojin Kwon <raccoonhj33@gmail.com>
2020-05-23 00:46:46 +0900

    200522 signin view

--- app/members/urls.py
+++ app/members/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from members import views
 
+app_name = 'members'
 urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
--- app/members/views.py
+++ app/members/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.core.exceptions import ValidationError
+from django.shortcuts import render, redirect
 
 
 def signin(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('main:fridge')
+        if ValidationError:
+            error_msg = 'Email 혹은 Password가 틀립니다.'
+            context = {
+                'error_msg': error_msg,
+            }
+            return render(request, 'members/signin.html', context)
     return render(request, 'members/signin.html')
 
 
